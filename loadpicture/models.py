cf7322ae31d9e46e03b6ed3508ba920603a24894
liuzw <liuzw@PC-201302061544.zhonghe.com>
2013-05-06 10:08:19 +0800

    add mesbook for commit

--- loadpicture/models.py
+++ loadpicture/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Mesbook(models.Model):
+    mestext = models.CharField(max_length=30)
+    mestime = models.CharField(max_length=50)
+    def __unicode__(self):
+        return self.mestext
