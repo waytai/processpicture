37aa975375af42a406e4c7d53e29b66bf39fd448
liuzw <liuzw@PC-201302061544.zhonghe.com>
2013-05-09 12:26:25 +0800

    in views add process

--- loadpicture/views.py
+++ loadpicture/views.py
@@ -2,9 +2,10 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-from django.http import HttpResponseRedirect
-import Image , ImageFile, os
+from django.http import HttpResponseRedirect , HttpResponse
+import Image , os
 from picture import settings
+import ImageFile
 
 # this is for uploading picture
 @csrf_exempt
@@ -25,5 +26,16 @@ def upload(request):
         return render_to_response('load_image.html' , context_instance=RequestContext(request))
     else:
         return HttpResponseRedirect("/load_image/")
+
+@csrf_exempt
+def process_picture(request):
+    pic_name = os.path.join(settings.Img_dir , "liu.jpg")
+    new_width =  100
+    new_height = 200
+    pic_img = Image.open(pic_name)
+    pic_img = pic_img.resize((new_width , new_height) , Image.ANTIALIAS)
+    new_liu_path = pic_name.replace("liu" , "new_liu")
+    pic_img.save(new_liu_path)
+    return HttpResponse("/load_image/")
     
     
